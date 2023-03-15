3b0cadd5dee432308f987777853a6eaebfb1fb24
kennetholivas <kennetholivas1@gmail.com>
2023-03-15 15:37:19 -0600

    fix: delete extra user option

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -3,14 +3,12 @@ import type { NextPage } from "next";
 import React, { useMemo, useState } from "react";
 import type { ColumnDef } from "@tanstack/react-table";
 import { api } from "@/utils/api";
-import { PlusIcon, Cog6ToothIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/24/outline";
 import Modal from "@/components/UI/Modal/Index";
 import AddUserForm from "@/components/User/AddUserForm";
 import type { User } from "@prisma/client";
 import { useRouter } from "next/router";
 import Breadcrumbs from "@/components/UI/Breadcrumbs";
-import Link from "next/link";
-import FieldUserButton from "@/utils/FieldData/FieldUserButton";
 import HeaderTitle from "@/components/UI/HeaderTitle";
 import Loader from "@/components/Loader";
 import UserReport from "@/components/Reports/UserReport";
@@ -66,17 +64,7 @@ const index: NextPage = () => {
       <div className="max-w-screen-4xl mx-auto mb-4 px-2">
         <HeaderTitle title="Users">
           <div className="flex space-x-4">
-            <FieldUserButton onFinishQuery={refetch} />
-            <Link
-              href="/roles"
-              className="flex rounded-md bg-yellow-600 px-4 py-2 text-white/90 shadow-lg shadow-yellow-600/40 transition-all duration-200 hover:bg-yellow-500 hover:text-white"
-            >
-              Roles
-              <Cog6ToothIcon className="ml-2 h-6 w-6" />
-            </Link>
-
             <UserReport />
-
             <button
               onClick={openModal}
               className="flex rounded-md bg-indigo-600 px-4 py-2 text-white/90 shadow-lg shadow-indigo-600/40 transition-all duration-200 hover:bg-indigo-500 hover:text-white"
